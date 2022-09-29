bbcc0c3efa11d17328e32fc59d692592759c8a68
sabakoguashvili16 <sabakoguashvili16@gmail.com>
2022-09-29 22:21:20 +0400

    delete uneccesry code

--- Collections/ConsoleApp1/Program.cs
+++ Collections/ConsoleApp1/Program.cs
@@ -1,18 +1,1 @@
-﻿using ConsoleApp1;
-
-MyList<string> list = new MyList<string>();
-list.AddItem("1");
-list.AddItem("2");
-list.AddItem("3");
-list.AddItem("4");
-list.AddItem("5");
-list.AddItem("6");
-list.AddItem("7");
-list.AddItem("8");
-
-foreach (var item in list)
-{
-
-}
-
-Console.WriteLine(list.First());
+﻿Console.WriteLine("Hello World!");
